2e4aba1c40988a4d6368ea3a8786d4b950d1033c
Manuhin S Palath <manuhin.palath@ccsdev01.onmicrosoft.com>
2024-01-18 15:11:55 +0530

    test

--- src/webparts/birthdays/components/Birthdays.tsx
+++ src/webparts/birthdays/components/Birthdays.tsx
@@ -21,7 +21,6 @@ export default class Birthdays extends React.Component<IBirthdaysProps, {}> {
           <div>Web part property value: <strong>{escape(description)}</strong></div>
         </div>
         <div>
-          <h3>Welcome to SharePoint Framework!</h3>
           <p>
             The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
           </p>
